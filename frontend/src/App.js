5ecee5236deed54dcb3f6aedeb1e81a3e54896c1
yccjs <yccjsalvador@gmail.com>
2022-05-08 16:16:31 +0900

    chore: require cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Feed from "./Feed";
 import NewPost from "./NewPost";
+import axios from "axios";
 
 function App() {
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8080/posts");
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <Navbar />
