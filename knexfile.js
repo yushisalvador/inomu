fe1227c422d17b6f2178beb995d7047c3c060aee
yccjs <yccjsalvador@gmail.com>
2022-05-10 22:09:02 +0900

    deploy: deploy frontend to firebase

--- knexfile.js
+++ knexfile.js
@@ -2,33 +2,6 @@ require("dotenv").config({
   path: "./.env",
 });
 
-// module.exports = {
-//   development: {
-//     client: "pg",
-//     connection: process.env.DATABASE_URL || {
-//       database: process.env.DB_NAME,
-//       port: process.env.DB_PORT || 8080,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//     },
-//     migrations: {
-//       directory: "./server/db/migrations",
-//     },
-//     seeds: { directory: "./server/db/seeds" },
-//   },
-//   production: {
-//     client: "pg",
-//     connection: {
-//       connectionString: process.env.DATABASE_URL,
-//       ssl: { rejectUnauthorized: false },
-//     },
-//     migrations: {
-//       directory: "./server/db/migrations",
-//     },
-//     seeds: { directory: "./server/db/seeds" },
-//   },
-// };
-
 module.exports = {
   client: "pg",
   connection: process.env.DATABASE_URL || {
